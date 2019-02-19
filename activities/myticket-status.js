99da82766bda3eeda2d835eacfa6575ffca12424
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-02-19 16:20:38 +0100

    Changed indentation from 4 spaces to 2.

--- activities/myticket-status.js
+++ activities/myticket-status.js
@@ -5,37 +5,35 @@ const handleError = require('@adenin/cf-activity').handleError;
 const api = require('./common/api');
 
 module.exports = async (activity) => {
-    try {
-        api.initialize(activity);
+  try {
+    api.initialize(activity);
 
-        const response = await api('/issues?state=opened&filter=assigned');
+    const response = await api('/issues?state=opened&filter=assigned');
 
-        let assignedIssuesUrl = 'https://github.com/issues/assigned';
+    let assignedIssuesUrl = 'https://github.com/issues/assigned';
 
-        let ticketStatus = {
-            title: 'Open Tickets',
-            url: assignedIssuesUrl,
-            urlLabel: 'All tickets',
-        };
+    let ticketStatus = {
+      title: 'Open Tickets',
+      url: assignedIssuesUrl,
+      urlLabel: 'All tickets',
+    };
 
-        if (response.body.length != 0) {
-            ticketStatus = {
-                description: `You have ${response.body.length} tickets assigned`,
-                color: 'blue',
-                value: response.body.length,
-                actionable: true
-            }
-        } else {
-            ticketStatus = {
-                title: 'Open Tickets',
-                description: `You have no tickets assigned`,
-                actionable: false
-            }
-        }
-
-        activity.Response.Data = ticketStatus;
-
-    } catch (error) {
-        handleError(error, activity);
+    if (response.body.length != 0) {
+      ticketStatus = {
+        description: `You have ${response.body.length} tickets assigned`,
+        color: 'blue',
+        value: response.body.length,
+        actionable: true
+      }
+    } else {
+      ticketStatus = {
+        actionable: false
+      }
     }
+
+    activity.Response.Data = ticketStatus;
+
+  } catch (error) {
+    handleError(error, activity);
+  }
 };
