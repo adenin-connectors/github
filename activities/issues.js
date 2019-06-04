84c11fb2bcce38b6c7c5099d4f529278d2a01d36
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-04 12:14:23 +0200

    Issue cleanup.

--- activities/issues.js
+++ activities/issues.js
@@ -4,13 +4,34 @@ const api = require('./common/api');
 module.exports = async function (activity) {
   try {
     api.initialize(activity);
-    let pagination = $.pagination(activity);
-    const response = await api(`/issues?filter=all&state=open`);
-
+    let allIssues = [];
+    var dateRange = $.dateRange(activity, "today");
+    let page = 1;
+    let maxResults = 100;
+    let response = await api(`/issues?filter=all&state=open&since=${dateRange.startDate}&page=${page}&per_page=${maxResults}`);
     if ($.isErrorResponse(activity, response)) return;
+    allIssues.push(...response.body);
 
-    activity.Response.Data.items = api.convertIssues(response.body);
-    let value = activity.Response.Data.items.items.length;
+    let hasMore = false;
+    if (response.body.length == maxResults) {
+      hasMore = true;
+    }
+
+    while (hasMore) {
+      page++;
+      response = await api(`/issues?filter=all&state=open&since=${dateRange.startDate}&page=${page}&per_page=${maxResults}`);
+      if ($.isErrorResponse(activity, response)) return;
+      allIssues.push(...response.body);
+      if (response.body.length != maxResults) {
+        hasMore = false;
+      }
+    }
+
+    let value = allIssues.length;
+    let pagination = $.pagination(activity);
+    let pagiantedItems = paginateItems(allIssues, pagination);
+
+    activity.Response.Data.items = api.convertIssues(pagiantedItems);
     activity.Response.Data.title = T(activity, 'Open Issues');
     activity.Response.Data.link = 'https://github.com/issues/assigned';
     activity.Response.Data.linkLabel = T(activity, 'All Issues');
@@ -28,3 +49,20 @@ module.exports = async function (activity) {
     $.handleError(activity, error);
   }
 };
+
+//** paginate items[] based on provided pagination */
+function paginateItems(items, pagination) {
+  let pagiantedItems = [];
+  const pageSize = parseInt(pagination.pageSize);
+  const offset = (parseInt(pagination.page) - 1) * pageSize;
+
+  if (offset > items.length) return pagiantedItems;
+
+  for (let i = offset; i < offset + pageSize; i++) {
+    if (i >= items.length) {
+      break;
+    }
+    pagiantedItems.push(items[i]);
+  }
+  return pagiantedItems;
+}
