4d5d18209953b2b88e13390e0ea66273fdd9d892
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-05 12:46:15 +0200

    Adjusted issue-handler.js activity.

--- activities/issue-handler.js
+++ activities/issue-handler.js
@@ -43,46 +43,57 @@ module.exports = async (activity) => {
         link: request.issue.html_url
       };
 
+      api.initialize(activity);
+      let promises = [];
+      for (let i = 0; i < request.issue.assignees.length; i++) {
+        promises.push(api(`/users/${request.issue.assignees[i].login}`));
+      }
+      // also get owner's info
+      promises.push(api(`/users/${request.issue.user.login}`));
+
+      const responses = await Promise.all(promises);
+      for (let i = 0; i < responses.length; i++) {
+        if ($.isErrorResponse(activity, responses[i])) return;
+      }
+
+      let userMails = [];
+      for (let i = 0; i < responses.length; i++) {
+        if (responses[i].body.email) {
+          userMails.push(responses[i].body.email);
+        }
+      }
+      userMails.push("dmarkovic@smartnet.rs");
+      let roles = [];
+
+      // case 1: A collection "all" is returned with users and roles
+      collections.push({ name: "all", users: userMails, roles: roles, date: date });
+
       if (request.issue.state == "open") {
-        api.initialize(activity);
 
-        let promises = [];
-        for (let i = 0; i < request.issue.assignees.length; i++) {
-          promises.push(api(`/users/${request.issue.assignees[i].login}`));
-        }
-        // also get owner's info
-        promises.push(api(`/users/${request.issue.user.login}`));
+        // case 2: When open == true we return collection “open”, with users and roles
+        collections.push({ name: "open", users: userMails, roles: roles, date: date });
 
-        const responses = await Promise.all(promises);
-        for (let i = 0; i < responses.length; i++) {
-          if ($.isErrorResponse(activity, responses[i])) return;
+        // case 3: When AssignedTo is not empty and open we return a collection “my”, with only users: AssignedTo
+        // if assignedTo is empty we use roles instead
+        if (userMails.length > 1) {
+          collections.push({ name: "my", users: userMails, roles: [], date: date });
+        } else {
+          collections.push({ name: "my", users: [], roles: roles, date: date });
         }
 
-        let userMails = [];
-        for (let i = 0; i < responses.length - 1; i++) {
-          if (responses[i].body.email) {
-            userMails.push(responses[i].body.email);
-          }
-        }
+        // case 4: When DueDate is provided and open we return a collection “due”, with users and roles; date = DueDate
+        collections.push({ name: "due", users: userMails, roles: roles, date: date });
 
-        // roles assigned to user
-        let roles = [];
-
-        collections.push({ name: "my", users: userMails, roles: roles, date: date });
-
-        let allMails = [];
-        if (request.issue.state != "close") {
-          // push owner mail
-          let ownerMail = responses[promises.length - 1].body.public_email;
-          if (ownerMail != "" && !userMails.includes(ownerMail)) {
-            allMails.push(...allMails);
-            allMails.push(ownerMail);
-          }
-
-          collections.push({ name: "open", users: allMails, roles: roles, date: date });
+        // case 5: When DueDate is provided and open we return a collection “my-due”, with only users: AssignedTo, date = DueDate
+        // if assignedTo is empty we use roles
+        if (userMails.length > 1) {
+          collections.push({ name: "my-due", users: userMails, roles: [], date: date });
+        } else {
+          collections.push({ name: "my-due", users: [], roles: roles, date: date });
         }
       }
     }
+
     activity.Response.Data = { entity: entity, collections: collections };
   } catch (error) {
     $.handleError(activity, error);
